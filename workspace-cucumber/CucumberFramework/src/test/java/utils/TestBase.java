34f58d00968280c01cb51214a28d9192c8d99c3b
borjaherranzmoral <33319229+borjaherranz@users.noreply.github.com>
2025-12-29 14:03:10 +0100

    Refactor WebDriver setup for Chrome and Firefox

--- workspace-cucumber/CucumberFramework/src/test/java/utils/TestBase.java
+++ workspace-cucumber/CucumberFramework/src/test/java/utils/TestBase.java
@@ -37,19 +37,14 @@ public class TestBase {
 		
 		if (driver == null) {
 			if(result.equalsIgnoreCase("chrome")) {
-				System.setProperty(
-						"webdriver.chrome.driver",
-						System.getProperty("user.dir") +
-						"//src//test//resources//chromedriver.exe"
-						);
-				
+				WebDriverManager.chromeDriver().setup();
+								
 				ChromeOptions options = new ChromeOptions();
 				options.addArguments("--remote-allow-origins=*");
 				driver = new ChromeDriver(options);
-				
-				
-			} else if(result.equals("firefox")) {
-				// Add the driver from internet 
+			} else if(result.equalsIgnoreCase("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
 			}
 			implicitWait();
 			driver.get(url);

834e128325ef14efb1065b39488e2981fa3bd2f0
borjaherranzmoral <33319229+borjaherranz@users.noreply.github.com>
2025-12-29 14:06:02 +0100

    Add Firefox support to TestBase class

--- workspace-cucumber/CucumberFramework/src/test/java/utils/TestBase.java
+++ workspace-cucumber/CucumberFramework/src/test/java/utils/TestBase.java
@@ -8,53 +8,59 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
-	
-	// Attributes
-	public WebDriver driver;
-	
-	public WebDriver webDriverManager() throws IOException {
-		System.out.println(System.getProperty("user.dir"));
-		
-		/** We convert the properties in a FileInputStream before sending to the properties */
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + 
-				"//src//test//resources//global.properties"
-				);
-		
-		Properties prop = new Properties();
-		prop.load(fis);
-		
-		/** Get the properties from the file */
-		String browser = (String) prop.get("browser");
-		String url = prop.getProperty("QAUrl");
-		
-		/** In case we inject the browser by the commands line */
-		String browser_maven = System.getProperty("browser");
-		
-		String result = browser_maven!=null? browser_maven : browser;
-		
-		if (driver == null) {
-			if(result.equalsIgnoreCase("chrome")) {
-				WebDriverManager.chromeDriver().setup();
-								
-				ChromeOptions options = new ChromeOptions();
-				options.addArguments("--remote-allow-origins=*");
-				driver = new ChromeDriver(options);
-			} else if(result.equalsIgnoreCase("firefox")) {
-				WebDriverManager.firefoxdriver().setup();
+
+    public WebDriver driver;
+
+    public WebDriver webDriverManager() throws IOException {
+
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
+
+        FileInputStream fis = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/global.properties"
+        );
+
+        Properties prop = new Properties();
+        prop.load(fis);
+
+        String browserFromFile = prop.getProperty("browser");
+        String url = prop.getProperty("QAUrl");
+
+        // Maven parameter has priority
+        String browserFromMaven = System.getProperty("browser");
+        String browser = browserFromMaven != null ? browserFromMaven : browserFromFile;
+
+        if (driver == null) {
+
+            if (browser.equalsIgnoreCase("chrome")) {
+
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+				
+                if (System.getenv("JENKINS_HOME") != null) {
+                    options.addArguments("--headless=new");
+                    options.addArguments("--disable-gpu");
+                    options.addArguments("--window-size=1920,1080");
+                }
+
+                driver = new ChromeDriver(options);
+
+            } else if (browser.equalsIgnoreCase("firefox")) {
+
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
-			}
-			implicitWait();
-			driver.get(url);
-		}
+            }
 
-		return driver;
-	}
-	
-	public void implicitWait() {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	}
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.get(url);
+        }
 
+        return driver;
+    }
 }
